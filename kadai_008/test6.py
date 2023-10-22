52d361da4ca6ba55d88abb6225acfd3ce9b0e440
Yamahiro-Frandol <148076928+Yamahiro-Frandol@users.noreply.github.com>
2023-10-22 22:56:53 +0900

    Add files via upload

---
+++ kadai_008/test6.py
@@ -0,0 +1,16 @@
+import random
+
+var = random.randint(1,100)
+
+print(var)
+
+if var % 3 == 0:
+    print(var,"Fizz")
+elif var % 5 == 0:
+    print(var,"Buzz")
+if var % 15 == 0:
+    print(var,"FizzBUzz")
+else:
+    print(var)
+
+               
